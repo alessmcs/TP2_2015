212383dc64cfdc98c1b60f48b13897f0e2b0a83f
oceane-hays <128166831+oceane-hays@users.noreply.github.com>
2023-12-07 20:18:17 -0500

    update ;) !!

--- code/src/main/java/FileMaps.java
+++ code/src/main/java/FileMaps.java
@@ -1,2 +1,53 @@
-public class FileMaps<k, v> {
+
+public class FileMaps<k, v> implements Map<k,v>{
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean containsKey(k key) {
+        return false;
+    }
+
+    @Override
+    public v get(k key) {
+        return null;
+    }
+
+    @Override
+    public v put(k key, v value) {
+        return null;
+    }
+
+    @Override
+    public v remove(k key) {
+        return null;
+    }
+
+    @Override
+    public Iterable<k> keySet() {
+        return null;
+    }
+
+    @Override
+    public Iterable<v> values() {
+        return null;
+    }
+
+    @Override
+    public Iterable<Entry<k, v>> entrySet() {
+        return null;
+    }
+
+
+
+
+
 }
