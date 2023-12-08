d031ca25d9f75c52aeda586671f5a87e66b6c763
alessmcs <alessandramancas23@gmail.com>
2023-12-08 12:22:15 -0500

    eeeww

--- code/src/main/java/Main.java
+++ code/src/main/java/Main.java
@@ -12,38 +12,88 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         ArrayList<String[]> texteTraitee = TraitementDeTexte.traiterText("src/main/dataset");
-
+        ArrayList<String> nomsFichiers = TraitementDeTexte.nomFichier("src/main/dataset");
 
         BufferedReader br = new BufferedReader(new FileReader("src/main/query.txt"));
         ArrayList<String> listeCorrige = new ArrayList<>();
         String line;
+
         while ((line = br.readLine()) != null) {
 //            String correctLine = TraitementDeTexte.correction(line);
 //            listeCorrige.add(correctLine);
         }
+//        int j = 0;
+//        WordMap<String, Integer> myMap = new WordMap<>(331,57);
+//        int i = 0;
+//
+//        for (String nom : nomsFichiers) {
+//            for(String mot: texteTraitee.get(i)){
+//                if (myMap.containsKey(mot)){
+//                    FileMaps<String, ArrayList<Integer>> myMap2 = myMap.get(mot);
+//
+//                    if(myMap2.containsKey(nom)){ // si le nom du fichier existe
+//                        myMap2.get(nom).add(i);
+//                    } else {
+//                        myMap2.put(nom,new ArrayList<>(Arrays.asList(i)) );
+//                    }
+//                } else {
+//                    myMap.put(mot, new FileMaps<String, ArrayList<Integer>>());
+//                    FileMaps<String, ArrayList<Integer>> myMap2 = myMap.get(mot);
+//                    myMap2.put(nom,new ArrayList<>(Arrays.asList(i)) );
+//                }
+//                i++;
+//            }
+//        }
 
-        String nomFichier = "900.txt";
-        // test sur le premier fichier 900.txt
-        WordMap<String, Integer> myMap = new WordMap<>();
-        int i = 0; //index dans le string
-        for(String mot: texteTraitee.get(0)){
-            System.out.println(mot + ": " + WordMap.hashValue(mot));
-            if (myMap.containsKey(mot)){
-                FileMaps<String, ArrayList<Integer>> myMap2 = myMap.get(mot);
-                myMap2.get(nomFichier).add(i);
-            } else {
-                myMap.put(mot, new FileMaps<String, ArrayList<Integer>>());
-                FileMaps<String, ArrayList<Integer>> myMap2 = myMap.get(mot);
-                myMap2.put(nomFichier,new ArrayList<>(Arrays.asList(i)) );
+        WordMap<String, FileMaps<String, ArrayList<Integer>>> myMap = new WordMap<>(331, 57);
+        int i = 0;
+
+        for (String nom : nomsFichiers) {
+            int fileIndex = 0; // Reset index for each file
+
+            for (String mot : texteTraitee.get(fileIndex)) {
+                if (myMap.containsKey(mot)) {
+                    FileMaps<String, ArrayList<Integer>> myMap2 = myMap.get(mot);
+                    if (myMap2.containsKey(nom)) {
+                        myMap2.get(nom).add(fileIndex);
+                    } else {
+                        myMap2.put(nom, new ArrayList<>(Arrays.asList(fileIndex)));
+                    }
+                } else {
+                    FileMaps<String, ArrayList<Integer>> fileMap = new FileMaps<>();
+                    ArrayList<Integer> positions = new ArrayList<>(Arrays.asList(fileIndex));
+                    fileMap.put(nom, positions);
+                    myMap.put(mot, fileMap);
+                }
+                fileIndex++; 
             }
-            System.out.println(mot + " , " + myMap.get(mot).get(nomFichier).toString());
             i++;
         }
 
-//        for (String key : myMap.keySet()) {
-//            System.out.println(key);
+        for (String key : myMap.keySet()) {
+            System.out.println(key); // mot
+            for(String key2 : myMap.get(key).keySet() ){
+                System.out.println(key2 + ": " + myMap.get(key).get(key2) );
+            }
+        }
+
+//        WordMap<String, Integer> myMap = new WordMap<>(331,57);
+//        int i = 0; //index dans le string
+//        for(String mot: texteTraitee.get(0)){
+//            if (myMap.containsKey(mot)){
+//                FileMaps<String, ArrayList<Integer>> myMap2 = myMap.get(mot);
+//                myMap2.get("900.txt").add(i);
+//            } else {
+//                myMap.put(mot, new FileMaps<String, ArrayList<Integer>>());
+//                FileMaps<String, ArrayList<Integer>> myMap2 = myMap.get(mot);
+//                myMap2.put("900.txt",new ArrayList<>(Arrays.asList(i)) );
+//            }
+//            i++;
 //        }
+//
+//        for (String key : myMap.keySet()) {
+//                System.out.println(key + ", " + myMap.get(key).get("900.txt"));
+//            }
+//    }
 
-    }
-
-}
+}}
--- code/src/main/java/WordMap.java
+++ code/src/main/java/WordMap.java
@@ -1,8 +1,31 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class WordMap<K,V> extends ChainHashMap<String, FileMaps<String, ArrayList<Integer>>> {
 
     // K -> mot & V -> ref à FileMap
+
+    protected int n = 0; // number of entries in the map
+    protected static int prime;
+    protected static int capacity; // size of the table
+    private static long scale;
+    private static long shift; // shift and scale factors
+
+
+    private UnsortedTableMap<String,FileMaps<String, ArrayList<Integer>>>[] table; // initialized in createTable
+
+//    public WordMap() { super(); }
+//    public WordMap( int cap ) { super( cap ); }
+//    public WordMap( int cap, int p ) { super( cap, p ); }
+
+    public WordMap( int cap, int p ) {
+        this.prime = p;
+        this.capacity = cap;
+        Random rand = new Random();
+        this.scale = rand.nextInt( prime - 1 ) + 1;
+        this.shift = rand.nextInt( prime );
+        this.createTable();
+    }
 
     // TODO: citer ce code!
     public static int hashCode(String mot) {
@@ -17,9 +40,46 @@ public class WordMap<K,V> extends ChainHashMap<String, FileMaps<String, ArrayLis
 
     // fonction de compression
     public static int hashValue(String key){
-        int a = 5; int b = 13; int p = 577;
-        return ( (a*hashCode(key) + b)%p)%331; // MAD method, N = 331 (prime number)
+//        int a = 5; int b = 13;
+//        return ( (a*hashCode(key) + b)%p)%capacity; // MAD method
+        return (int)( ( Math.abs( key.hashCode() * scale + shift ) % prime ) % capacity );
     }
+
+    @Override
+    public FileMaps<String, ArrayList<Integer>> put( String key, FileMaps<String, ArrayList<Integer>> value ) {
+        FileMaps<String, ArrayList<Integer>> retV = bucketPut( hashValue( key ), key, value );
+        if( n > capacity * 0.75 ) // keep load factor <= 0.75
+            resize( 2 * capacity + 1 ); // (or find a nearby prime)
+        return retV;
+    }
+
+    @Override
+    protected void createTable() {
+        table = (UnsortedTableMap<String,FileMaps<String, ArrayList<Integer>>>[]) new UnsortedTableMap[this.capacity];
+    }
+
+    public boolean containsKey( String key ) { return bucketGet( hashValue( key ), key ) != null; }
+
+    @Override
+    protected FileMaps<String, ArrayList<Integer>> bucketPut( int h, String k, FileMaps<String, ArrayList<Integer>> v ) {
+        UnsortedTableMap<String,FileMaps<String, ArrayList<Integer>>> bucket = table[h];
+
+        if( bucket == null )
+            bucket = table[h] = new UnsortedTableMap<>();
+
+        int oldSize = bucket.size();
+        FileMaps<String, ArrayList<Integer>> old = bucket.put( k, v );
+        this.n += ( bucket.size() - oldSize ); // size may have increased
+        return old;
+    }
+
+    protected FileMaps<String, ArrayList<Integer>> bucketGet( int h, String k ) {
+        UnsortedTableMap<String,FileMaps<String, ArrayList<Integer>>> bucket = table[h];
+        if( bucket == null ) return null;
+        return bucket.get( k );
+    }
+
+    public FileMaps<String, ArrayList<Integer>> get( String key ) { return bucketGet( hashValue( key ), key ); }
 
     private void resize( int newCap ) {
         ArrayList<Entry<String, FileMaps<String, ArrayList<Integer> > >> buffer = new ArrayList<>();
@@ -30,6 +90,15 @@ public class WordMap<K,V> extends ChainHashMap<String, FileMaps<String, ArrayLis
         this.n = 0; // wil be recomputed while reinserting entries
         for( Entry<String, FileMaps<String, ArrayList<Integer>>> e : buffer )
             put((String) e.getKey(), e.getValue());
+    }
+
+    public Iterable<Entry<String, FileMaps<String, ArrayList<Integer>>>> entrySet() {
+        ArrayList<Entry<String, FileMaps<String, ArrayList<Integer>>>> buffer = new ArrayList<>();
+        for( int h = 0; h < this.capacity; h++ )
+            if( table[h] != null )
+                for( Entry<String, FileMaps<String, ArrayList<Integer>>> entry : table[h].entrySet() )
+                    buffer.add( entry );
+        return buffer;
     }
 
 }
